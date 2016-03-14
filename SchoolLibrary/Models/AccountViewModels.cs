7bc4c72075eba092b1cd85e7f09382e4eda23129
紅茶妖怪 <neko3000@live.com>
2016-03-14 23:27:15 +0900

    working with popularbooks

--- SchoolLibrary/Models/AccountViewModels.cs
+++ SchoolLibrary/Models/AccountViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace SchoolLibrary.Models
 {
@@ -65,16 +66,13 @@ namespace SchoolLibrary.Models
     public class RegisterViewModel
     {
         [Required]
-        [Display(Name = "账户名")]
-        public string UserName { get; set; }
-
-        [Required]
         [EmailAddress]
         [Display(Name = "电子邮件")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+        //[RegularExpression(@"[a-zA-Z0-9]*", ErrorMessage ="WHO ARE YOU")]
         [DataType(DataType.Password)]
         [Display(Name = "密码")]
         public string Password { get; set; }
--- SchoolLibrary/Models/Book.cs
+++ SchoolLibrary/Models/Book.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolLibrary.Models
 {
     public class Book
     {
         public virtual int Id { get; set; }
+        [Display(Name ="标题")]
         public virtual string Title { get; set; }
         public virtual string Description { get; set; }
         public virtual string Author { get; set; }
